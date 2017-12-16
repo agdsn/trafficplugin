6aa2eb9cc9992468ec05d32b389b8d9e258239b3
Florian Lamprecht <florian_lamprecht@gmx.de>
2017-12-16 13:51:15 +0100

    Rename some variables to better reflect their meaning and remove unused ones

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 var usedTraffic = null,
-	trafficVolumePerDay = 3 * 1024 * 1024,
-	trafficVolume = trafficVolumePerDay * 7;
+	quotaPerDay = 3 * 1024 * 1024,
+	maxQuota = quotaPerDay * 21;
 
 /** Resets all modified values to their defaults. */
 function clearState() {
@@ -16,11 +16,9 @@ function updateTraffic(){
 			return;
 		}
 		var quota = parseFloat(data.quota);
-		var proc = Math.round(quota / trafficVolume * 100);
+		var proc = Math.round(quota / maxQuota * 100);
 		if(proc > 100) proc = 100;
 		var name = proc - (proc % 5);
-		var ind = parseFloat(data.traffic["in"]);
-		var outd = parseFloat(data.traffic["out"]);
 
 		chrome.browserAction.setIcon({path: "icon/" + name + ".png"});
 
